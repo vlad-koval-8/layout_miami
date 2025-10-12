da419da73c9fe3e3321383a043f8ab8aba510dec
Vladyslav Koval <vladkoval0810@gmail.com>
2025-10-12 15:51:42 +0200

    make build

---
+++ layout_miami.1f87940a.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=layout_miami.1f87940a.js.map
--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
